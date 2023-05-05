420bb31f68de31702bb7d83db6e355ea112f474b
Ilya Kutilin <ilyakutilin@yandex.ru>
2023-05-05 17:00:55 +0300

    Basic list of Requesters

--- translation/orders/views.py
+++ translation/orders/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import get_object_or_404, redirect, render
+from orders.utils.paginator import pagination
 
 from . import forms, models
+
+REQUESTER_LIST_LIMIT = 50
 
 
 def index(request):
@@ -26,3 +29,10 @@ def edit_requester(request, requester_id):
 
     context = {"is_edit": True, "form": form, "requester": requester}
     return render(request, "orders/add_requester.html", context)
+
+
+def list_requesters(request):
+    requester_list = models.Requester.objects.all()
+    page_obj = pagination(request, requester_list, REQUESTER_LIST_LIMIT)
+    context = {"page_obj": page_obj}
+    return render(request, "orders/list_requesters.html", context)
